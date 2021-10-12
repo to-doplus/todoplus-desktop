4034895b30cdc85101bd10668f3cef1705f27224
Miroslav Šafář <miroslav.safar@znojmo.cz>
2021-10-12 16:39:52 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { Fragment, ReactElement } from "react"
+
+const App = () : ReactElement => {
+    return (
+        <Fragment>
+            <div>
+                Hello World from React!
+            </div>
+        </Fragment>
+    )
+}
+
+export default App
