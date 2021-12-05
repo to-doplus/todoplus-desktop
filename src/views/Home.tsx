3aafdfd76269457a735420b358e7fb0157322200
Mischulee <xparil04@vutbr.cz>
2021-12-05 15:58:30 +0100

    Homepage finished

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -4,6 +4,7 @@ import Layout from "../components/layout/Layout";
 import TaskListList from "../components/layout/TaskListList";
 import TaskDetails from "../components/taskDetails/TaskDetails";
 import NewTaskListButton from "../components/layout/NewTaskListButton"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Home = (): ReactElement => {
     return (
@@ -13,9 +14,17 @@ const Home = (): ReactElement => {
                 <h2>I made a huge to do list for today. I just can't figure out who's going to do it.</h2>
                 <div className="subtitles">
                     <h3>Getting started:</h3>
-                    <NewTaskListButton/>
+                    <div className="newTaskList">
+                        <NewTaskListButton />
+                    </div>
+                    <h3>Current version:</h3>
+                    <span className="version">
+                        1.0.0. - {window.navigator.platform}
+                    </span>
                     <h3>Tips:</h3>
-                    Super important tips haha, changing color etc.
+                    <span className="tips">
+                        The color of your task list icon can be changed by clicking the left mouse button on the  <FontAwesomeIcon icon={["fas", "ellipsis-h"]} size={"lg"} /> icon and selecting color change option.
+                    </span>
                 </div>
             </div>
 
