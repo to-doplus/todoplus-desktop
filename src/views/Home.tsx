bea9bac15cec6d6fab2c4c992fc844f7411b2dc2
Mischulee <xparil04@vutbr.cz>
2021-12-05 04:52:08 +0100

    Minor changes

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -12,15 +12,10 @@ const Home = (): ReactElement => {
                 <h1>To-Do Plus</h1>
                 <h2>I made a huge to do list for today. I just can't figure out who's going to do it.</h2>
                 <div className="subtitles">
-                    <h3>Start</h3>
-                    <h4>New task list</h4><NewTaskListButton />
-                    <h3>Recent</h3>
-                    <Link to="/Important">
-                        Important tasks
-                    </Link>
-                    <Link to="/MyDay">
-                        My Day
-                    </Link>
+                    <h3>Getting started:</h3>
+                    <NewTaskListButton/>
+                    <h3>Tips:</h3>
+                    Super important tips haha, changing color etc.
                 </div>
             </div>
 
